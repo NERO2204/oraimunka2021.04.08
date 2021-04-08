04bf76d18e780fcf2da0a742657f1c26e7647a24
NERO2204 <78953795+NERO2204@users.noreply.github.com>
2021-04-08 11:10:11 +0200

    ujverzio

---
+++ FELADAT2021.04.07/src/feladat2021/pkg04/pkg07/FELADAT20210407.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package feladat2021.pkg04.pkg07;
+
+/**
+ *
+ * @author Imi
+ */
+public class FELADAT20210407 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
